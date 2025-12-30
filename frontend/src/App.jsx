d7ec746f0a8fb07baf3950043a8b4a330879f3c3
J Pooja <poojajr2508@gmail.com>
2025-12-30 13:51:40 +0530

    Frontend: Configured Vite proxy and cleaned up default files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Frontend Setup Complete</h1>
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
